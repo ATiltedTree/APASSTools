a5cd09f3d794f0168bedd33252ad2f7dbce21775
Tilmann Meyer <allescrafterx@gmail.com>
2020-05-12 08:21:43 +0200

    style: remove unused static plugin loading

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,6 @@
 
 #include <QApplication>
 
-#ifdef QT_STATIC
-#include <QtPlugin>
-#include <QtSvg>
-#ifdef WIN32
-Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin)
-Q_IMPORT_PLUGIN(QWindowsVistaStylePlugin)
-#endif
-Q_IMPORT_PLUGIN(QSvgPlugin)
-#endif
-
 int main(int argc, char* argv[]) {
   QApplication a(argc, argv);
   APASSTools::Ref w = APASSTools::New(nullptr);
