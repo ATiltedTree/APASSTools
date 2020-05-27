c15b1e4323eff826e43a063f4cd6ad74521da8bf
Tilmann Meyer <allescrafterx@gmail.com>
2020-05-27 15:41:03 +0200

    fix: Fix using wrong var for convert

--- cmake/BuildIcon.cmake
+++ cmake/BuildIcon.cmake
@@ -17,15 +17,17 @@ macro(build_icon target src icon_sizes)
         add_custom_command(
           OUTPUT "${ICON_FOLDER}/icon_${ICON_SIZE}x${ICON_SIZE}.png"
           COMMAND
-            "${CONVERT}" -background none -resize ${ICON_SIZE}x${ICON_SIZE}
-            "${src}" "${ICON_FOLDER}/icon_${ICON_SIZE}x${ICON_SIZE}.png"
+            "${ImageMagick_convert_EXECUTABLE}" -background none -resize
+            ${ICON_SIZE}x${ICON_SIZE} "${src}"
+            "${ICON_FOLDER}/icon_${ICON_SIZE}x${ICON_SIZE}.png"
         )
         math(EXPR DOUBLE_SIZE "${ICON_SIZE} * 2")
         add_custom_command(
           OUTPUT "${ICON_FOLDER}/icon_${ICON_SIZE}x${ICON_SIZE}@2.png"
           COMMAND
-            "${CONVERT}" -background none -resize ${DOUBLE_SIZE}x${DOUBLE_SIZE}
-            "${src}" "${ICON_FOLDER}/icon_${ICON_SIZE}x${ICON_SIZE}@2.png"
+            "${ImageMagick_convert_EXECUTABLE}" -background none -resize
+            ${DOUBLE_SIZE}x${DOUBLE_SIZE} "${src}"
+            "${ICON_FOLDER}/icon_${ICON_SIZE}x${ICON_SIZE}@2.png"
         )
         list(APPEND ICONS "${ICON_FOLDER}/icon_${ICON_SIZE}x${ICON_SIZE}.png"
              "${ICON_FOLDER}/icon_${ICON_SIZE}x${ICON_SIZE}@2.png"
@@ -50,8 +52,9 @@ macro(build_icon target src icon_sizes)
         add_custom_command(
           OUTPUT "${ICON_FOLDER}/icon_${ICON_SIZE}x${ICON_SIZE}.png"
           COMMAND
-            "${CONVERT}" -background none -resize ${ICON_SIZE}x${ICON_SIZE}
-            "${src}" "${ICON_FOLDER}/icon_${ICON_SIZE}x${ICON_SIZE}.png"
+            "${ImageMagick_convert_EXECUTABLE}" -background none -resize
+            ${ICON_SIZE}x${ICON_SIZE} "${src}"
+            "${ICON_FOLDER}/icon_${ICON_SIZE}x${ICON_SIZE}.png"
         )
         list(APPEND ICONS "${ICON_FOLDER}/icon_${ICON_SIZE}x${ICON_SIZE}.png")
       endforeach()
@@ -59,7 +62,8 @@ macro(build_icon target src icon_sizes)
       add_custom_command(
         OUTPUT "${ICON}"
         DEPENDS ${ICONS}
-        COMMAND "${CONVERT}" "${ICON_FOLDER}/*" "${ICON}"
+        COMMAND "${ImageMagick_convert_EXECUTABLE}" "${ICON_FOLDER}/*"
+                "${ICON}"
       )
     endif()
   endif()
