ff85efebe6cd66a14c9b5ab03b1e2afe88c4ba3b
Tilmann Meyer <allescrafterx@gmail.com>
2020-07-22 19:02:44 +0200

    feat: Now in rust

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "ui/APASSTools/APASSTools.hpp"
-#include <QApplication>
-
-auto main(int argc, char *argv[]) -> int {
-  QApplication a(argc, argv);
-  APASSTools apasstools = APASSTools(nullptr);
-  apasstools.show();
-  return QApplication::exec();
-}
--- src/ui/AboutDialog/AboutDialog.cpp
+++
@@ -1,8 +0,0 @@
-#include "AboutDialog.hpp"
-
-AboutDialog::AboutDialog(QWidget *parent)
-    : QDialog(parent), ui(new Ui::AboutDialog(this)) {
-  ui->setupUi();
-  connect(this->ui->buttonBox, &QDialogButtonBox::accepted, this,
-          &AboutDialog::accept);
-}
